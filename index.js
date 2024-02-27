1c31f1ac3c6b1e542c4f2cedcbe20253cf295968
vaishnaviraja14 <136197669+vaishnaviraja14@users.noreply.github.com>
2024-02-27 19:43:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var sidenav = document.querySelector(".side-navbar")
+
+
+function showNavbar()
+{
+    
+    sidenav.style.left = "0"
+
+}
+
+function closeNavbar()
+{
+sidenav.style.left="-60%"
+}
+
+var photo = document.querySelector(".pic")
+
+function showPic()
+{
+
+}
+
+function closePic()
+{
+    
+}
